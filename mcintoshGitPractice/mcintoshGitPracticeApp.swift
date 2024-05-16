9c8a74f5469e54cb5a92af6880fa823e5aef5d08
Dobbinci <davinlsj@gmail.com>
2024-05-16 12:52:11 +0900

    Initial Commit

---
+++ mcintoshGitPractice/mcintoshGitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcintoshGitPracticeApp.swift
+//  mcintoshGitPractice
+//
+//  Created by Dabin Lee on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct mcintoshGitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
