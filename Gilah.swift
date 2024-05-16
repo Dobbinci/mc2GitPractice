434586d9ec84b633e1ab2fd7f198093af5a91319
Gilah <kblack98@naver.com>
2024-05-16 15:18:56 +0900

    #1 feat: gilah.swift 생성

---
+++ Gilah.swift
@@ -0,0 +1,18 @@
+//
+//  Gilah.swift
+//  mcintoshGitPractice
+//
+//  Created by Geunhye on 5/16/24.
+//
+
+import SwiftUI
+
+struct Gilah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gilah()
+}
