42ce69b0bca4093c1db61c38f9ea20ce88bf7864
Dobbinci <davinlsj@gmail.com>
2024-05-16 15:15:14 +0900

    #5 feat: vinci.swift 생성

---
+++ mcintoshGitPractice/vinci.swift
@@ -0,0 +1,18 @@
+//
+//  vinci.swift
+//  mcintoshGitPractice
+//
+//  Created by Dabin Lee on 5/16/24.
+//
+
+import SwiftUI
+
+struct vinci: View {
+    var body: some View {
+        Text("Hello, Vinci!")
+    }
+}
+
+#Preview {
+    vinci()
+}
