90daedb892e6ab1ef492567d7f584c08dc882282
이예형 <iyehyoung@iyehyeong-ui-MacBookPro.local>
2024-05-16 15:15:21 +0900

    #4 feat: create Ye.swift

---
+++ mcintoshGitPractice/Ye.swift
@@ -0,0 +1,18 @@
+//
+//  Ye.swift
+//  mcintoshGitPractice
+//
+//  Created by 이예형 on 5/16/24.
+//
+
+import SwiftUI
+
+struct Ye: View {
+    var body: some View {
+        Text("Hello, Ye@!@!!")
+    }
+}
+
+#Preview {
+    Ye()
+}
